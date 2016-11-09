96fd97c06d70e62d47b54de3864a435037363333
yilin chai <chaiyilin@hotmail.com>
2016-11-09 22:23:23 +1100

    all

--- js/temp.js
+++ js/temp.js
@@ -1,13 +1,5 @@
-function showStudent(ssn) {
-    var student = db.get(ssn);
-    if (student !== null) {
-        document.querySelector(`#${elementId}`).innerHTML =
-            `${student.ssn},${student.firstname},${student.lastname}`
-    else
-        {
-            throw new Error('Student not found!');
-        }
-    }
-}
-
-showStudent('444-44-4444');
+_.chain(enrollment)
+    .filter(student => student.enrolled > 1)
+    .pluck('grade')
+    .average()
+    .value(); //-> 90
--- slides/JavaScript is as object-oriented as it is functional.md
+++
@@ -1,3 +0,0 @@
-JavaScript is as object-oriented as it is functional.
-
-Deciding where to draw the line depends on personal preference and the demands of the problem you’re tackling
